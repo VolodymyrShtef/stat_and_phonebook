fe5547b616144f8ec95fba4936edc0e6fca6baa8
VolodSh <vzujka.com.ua@gmail.com>
2020-12-01 18:53:26 +0200

    Updates

---
+++ precache-manifest.84da2dbfb729f4ca5a6ccdf2b35d3821.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "430c9ead958b39543be29f5db95b784b",
+    "url": "/VolodymyrShtef/stat_and_phonebook/index.html"
+  },
+  {
+    "revision": "5291cb1ed9126bc5afde",
+    "url": "/VolodymyrShtef/stat_and_phonebook/static/css/main.b19eb69c.chunk.css"
+  },
+  {
+    "revision": "233b02cfa22e6f9a2c25",
+    "url": "/VolodymyrShtef/stat_and_phonebook/static/js/2.6112b81a.chunk.js"
+  },
+  {
+    "revision": "5291cb1ed9126bc5afde",
+    "url": "/VolodymyrShtef/stat_and_phonebook/static/js/main.112ffb71.chunk.js"
+  },
+  {
+    "revision": "0560851bab003531ef6b",
+    "url": "/VolodymyrShtef/stat_and_phonebook/static/js/runtime-main.edd827d8.js"
+  }
+]);
